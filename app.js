df77dddb9911c7d006a0f916d2246aaca91356da
jackjack89 <yewhwee.ng.2012@sis.smu.edu.sg>
2016-02-01 21:19:05 +0800

    trial push
    push

---
+++ app.js
@@ -0,0 +1,95 @@
+angular.module('rtfmApp', ['firebase', 'ngRoute', 'monospaced.qrcode'])
+
+.constant('fb', {
+    url: 'https://feedmeweek5.firebaseio.com'
+})
+
+.config(function ($routeProvider) {
+    $routeProvider
+        .when('/threads', {
+            templateUrl: 'threads.html',
+            controller: 'threadsCtrl',
+            resolve: {
+                threadsRef: function (threadServ) {
+                    return threadServ.getThreads()
+                }
+            }
+        })
+        .when('/create', {
+            templateUrl: 'create.html',
+            controller: 'QRCodeCtrl',
+            resolve: {
+                threadsRef: function (threadServ) {
+                    return threadServ.getThreads()
+                }
+            }
+        })
+        .when('/threads/:threadId', {
+            templateUrl: 'thread.html',
+            controller: 'threadCtrl',
+            resolve: {
+                threadRef: function ($route, threadServ) {
+                    return threadServ.getThread($route.current.params.threadId)
+                },
+                commentsRef: function ($route, threadServ) {
+                    return threadServ.getComments($route.current.params.threadId)
+                }
+            }
+        })
+        .otherwise({
+            redirectTo: '/threads'
+        })
+})
+
+.controller('threadsCtrl', function ($scope, $firebaseArray, threadsRef) {
+    $scope.threads = $firebaseArray(threadsRef);
+    $scope.threads.$loaded().then(function (threads) {});
+})
+
+.controller("QRCodeCtrl", function ($scope, $firebaseArray, threadsRef) {
+    $scope.qrcodeString = 'business name here';
+    $scope.size = 250;
+    $scope.correctionLevel = '';
+    $scope.typeNumber = 0;
+    $scope.inputMode = '';
+    $scope.image = true;
+    
+    $scope.threads = $firebaseArray(threadsRef);
+    $scope.threads.$loaded().then(function (threads) {});
+    $scope.createThread = function (username, title) {
+        $scope.threads.$add({
+            username: username,
+            title: title
+        });
+    };
+
+})
+
+.controller('threadCtrl', function ($scope, $firebaseArray, $firebaseObject, threadRef, commentsRef) {
+    var thread = $firebaseObject(threadRef);
+    console.log("title is " + thread.title);
+    thread.$bindTo($scope, 'thread');
+    $scope.comments = $firebaseArray(commentsRef)
+    $scope.createComment = function (username, text) {
+        $scope.comments.$add({
+            username: username,
+            text: text
+        })
+    }
+})
+
+.service('threadServ', function (fb) {
+    this.getThreads = function () {
+        return new Firebase(fb.url + '/threads');
+    }
+    this.getThread = function (threadId) {
+        return new Firebase(fb.url + '/threads/' + threadId)
+    }
+    this.getComments = function (threadId) {
+        return new Firebase(fb.url + '/threads/' + threadId + '/comments')
+    }
+})
+
+.directive()
+
+;
